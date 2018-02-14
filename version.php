827fd48467a2721c3b114044c7792bfa90b10ebb
roglos <roelmann@glos.ac.uk>
2018-02-14 11:56:50 +0000

    bump version number

--- version.php
+++ version.php
@@ -31,6 +31,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018112900;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018521400;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'local_assessmentgrades';  // Full name of the plugin (used for diagnostics).
